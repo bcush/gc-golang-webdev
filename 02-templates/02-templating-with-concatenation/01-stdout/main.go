762b8400ff2ba5399166122b7684290df94e361d
Brian Cushman <bcushman@corpinfo.com>
2017-08-28 13:41:11 -0700

    added stuff

---
+++ 02-templates/02-templating-with-concatenation/01-stdout/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Brian Cushman"
+
+	tpl := `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<title>Hello World!</title>
+	</head>
+	<body>
+	<h1>` + name + `</h1>
+	</body>
+	`
+
+	fmt.Println(tpl)
+}
